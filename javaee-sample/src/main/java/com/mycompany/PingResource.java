c5ff74cdaad046f911ed9b3533cdd40b305ebe85
mauricio-wodarski <mauricio-wodarski@PRO-PAE-5533.PROCERGS.REDERS>
2018-06-07 17:14:14 -0300

    first commit

---
+++ javaee-sample/src/main/java/com/mycompany/PingResource.java
@@ -0,0 +1,14 @@
+package com.mycompany;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("ping")
+public class PingResource {
+	
+	@GET
+	public String get() {
+		return "Hello world!";
+	}
+
+}
